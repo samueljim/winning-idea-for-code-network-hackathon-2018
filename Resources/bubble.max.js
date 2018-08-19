4a785d25b7dfcff5f4338ba2b0e7f70317acf3ec
samueljim <samueljim@msn.com>
2018-08-19 12:54:48 +1000

    0.7.2

--- Resources/bubble.max.js
+++ Resources/bubble.max.js
@@ -1,56 +1,18 @@
+var a = [33, 103, 3, 726, 200, 984, 198, 764, 9];
+var bubbleSortRPzayJ_MIzrKhNeS_hasReturned = false, bubbleSortRPzayJ_MIzrKhNeS_return, arguments = [a];
+var a = arguments[0];
+var swapped;
+do {
+    swapped = false;
+    for (var i = 0; i < a.length - 1; i++) {
+        if (a[i] > a[i + 1]) {
+            var temp = a[i];
+            a[i] = a[i + 1];
+            a[i + 1] = temp;
+            swapped = true;
+        }
+    }
+} while (swapped);
 
-var bubbleSortecIxgBjuilZplQkB_return
-,
-
-swapped
-,
-a=[
-33
-,
-103
-,
-3
-,
-726
-,
-200
-,
-984
-,
-198
-,
-764
-,
-9
-] /* <-- array */
-,
-bubbleSortecIxgBjuilZplQkB_hasReturned=!1
-,
-arguments=[
-a
-] /* <-- array */
-
-;a=arguments[0]
-
-;do/* opening function --> */ {
-swapped=!1
-;for(
-var i=0;i<a.length-1;i++
-)/* opening function --> */ {
-if(
-a[i]>a[i+1]
-)/* opening function --> */ {
-var temp=a[i]
-;a[i]=a[i+1]
-,
-a[i+1]=temp
-,
-swapped=!0
-} /* <-- end of function */ 
-} /* <-- end of function */ 
-} /* <-- end of function */ while(
-swapped
-)
-;console.log(
-a
-);
+bubbleSortRPzayJ_MIzrKhNeS_return;
+console.log(a);
