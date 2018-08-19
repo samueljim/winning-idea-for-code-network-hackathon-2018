54a46f300db9f00111a2fb4dca02fef677bcdc95
Matthew Magin <matthew.d.magin@gmail.com>
2018-08-19 13:07:35 +1000

    Added disabled styles

--- public/script.js
+++ public/script.js
@@ -1,13 +1,8 @@
-var textarea = document.querySelector("input");
+var textarea = document.querySelector("textarea");
 textarea.addEventListener('keydown', autosize);
 
 function autosize(){
-    var el = this;
-    setTimeout(function(){
-        el.style.cssText = 'height:auto;padding:0;';
-        el.style.cssText = 'height:' + el.scrollHeight + 'px';
     
-    }, 0);
 }
 
 function sendData(){
